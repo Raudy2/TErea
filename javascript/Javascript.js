4b124b2c7db79baf05b416c7bab46c3791f85d65
Raudy beltre segura <maxbeltre@gmail.com>
2019-02-28 16:33:17 -0400

    first commit

---
+++ javascript/Javascript.js
@@ -0,0 +1,108 @@
+// alert("Hola Nigga")
+// //
+// var nombre = 'andres';
+// var edad = 21;
+// console.log('Usted se llama ' + nombre + ' Y tiene ' + edad + ' anos ');
+// var a = 2, b = 3, c = 2;
+// var resultado = a + b + c;
+// console.log(resultado);
+
+//Conversion de variable
+
+// var producto = 'camiseta';
+// var precio1 = 2500;
+// var precio2 = 5000;
+// var resultado = producto + precio1;
+// console.log(resultado);
+
+// var casado = true;
+// var nombre = 'pablo';
+// var resultado = casado + nombre;
+// console.log(resultado);
+
+ // var ahorros = 10000;
+ // var bancarrota = false;
+ // var resultado = ahorros + bancarrota;
+ // console.log(resultado);
+
+ // var numero = '23abc';
+ // numero = parseInt(numero);
+ // console.log(typeof(numero));
+
+// var numero2 = '1.23abc';
+// numero2 = parseFloat(numero2);
+// console.log(numero2);
+
+// funcion prompt
+
+// var edad = prompt('Escribe tu edad ', 'en numero solamente');
+// edad = parseInt(edad);
+// console.log(typeof(edad));
+
+// Estructura de control condicional (if, else, else if )
+
+// var edad = prompt('Escribe tu edad ', 'en numeros solamente ');
+// var ciudad = prompt('Escriba su ciudad de origen');
+// edad = parseInt(edad);
+//
+// if(edad > 18){
+//   console.log('Usted es mayor de edad ');
+// }
+// else if (ciudad == 'bogota') {
+//   console.log('Usted es de ' + ciudad);
+// }
+// else{
+//   console.log('Usted es menor chingado ');
+// }
+
+// switch
+// var fruta = prompt('Escribe tu fruta preferidad');
+// switch (fruta) {
+//   case 'mango':
+//   console.log('El mango es una de las mejores frutas que puedes disfrutar');
+//   break;
+//
+//   case 'manzana':
+//   console.log('La manzana es una fruta con muchas propiedades medicinales');
+//   break;
+//
+//   case 'banana':
+//   console.log('El banano es una rica fuente de potasio');
+//   break;
+//
+//   default:
+//   console.log('Esto no es una fruta o no se encuentra dentro de la lista ');
+//
+// }
+
+// switch numbers
+
+// var edad = prompt('Escribe tu edad', 'En numero solamente');
+// edad = parseInt(edad);
+//
+// switch (edad) {
+//   case 10:
+//   console.log('Eres un bebe');
+//   break;
+//
+//   case 15:
+//   console.log('Eres un adolecente');
+//   break;
+//
+//   case 21:
+//   console.log('Eres adulto bitch');
+//   break;
+//
+//   default:
+//   console.log('Eres un  adulto mayor');
+//
+// }
+
+// ciclo for
+
+// for(var r = 0; r <= 5; r += 2 ){
+//   console.log(r);
+// }
+for(var a = 10; a > 0; a -= 2){
+  console.log(a);
+}
